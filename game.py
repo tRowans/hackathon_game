45d56787f15b3bba6be31e229cec18dac744a1f6
Jake <Jake.tommey@gmail.com>
2018-04-07 12:03:39 -0700

    function Test_for_victory compares the chi result against tolerance for success and returns if user was successful. may absorb into Compare_stats at later time.

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ from pyquil.api import QVMConnection
 import numpy
 from scipy.stats import chisquare 
 
-def genQUIL(layers):
+def genQUIL(layers): #takes a list of gates in format [[H(1),I(2)],[H(2),I(1)],[CNOT(1,2),Y(3)]] converts to QUIL
 
     program = Program()
 
@@ -14,23 +14,30 @@ def genQUIL(layers):
 
     return program
 
-
-def compare_stats(dist_user,dist_ref): #function takes user and reference distributions and compares them.
+def Compare_stats(dist_user,dist_ref): #function takes user and reference distributions in format [0.8,0.4,0.7] and compares them.
 
     result = chisquare(dist_user,dist_ref)
     
-
     return result
 
+def Victory(result,tolerance):  #takes the results of stats test and the required tolerance of success 
+    success = False
+
+    if (result[1] > tolerance):
+        success = True
+    else:
+        success = False 
+
+    return success
+
+def jake_test():  #probably bad practise to put my test here but oh well.
 
 
+    test_user = [0.8,0.4,0.7]
+    test_ref = [0.6,0.3,0.2]
+    test_circuit = [[H(1),I(2)],[H(2),I(1)],[CNOT(1,2),Y(3)]]
 
-test_user = [0.8,0.4,0.7]
-test_ref = [0.78,0.39,0.71]
-
-test_circuit = [[H(1),I(2)],[H(2),I(1)],[CNOT(1,2),Y(3)]]
-
-prog = genQUIL(test_circuit)
-res = compare_stats(test_user,test_ref)
-print (prog)
-print(res)
+    prog = genQUIL(test_circuit)
+    res = Compare_stats(test_user,test_ref)
+    suc = Victory(res,0.95)
+    print (prog,res[1],suc)
