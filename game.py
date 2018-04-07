2a01c7f4bbea7958e3794efb799b4e3a434d00d2
Jake <Jake.tommey@gmail.com>
2018-04-07 13:03:59 -0700

    created function for user input of a circuit, takes the program and shows the user a randomised list of available gates

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ from pyquil.gates import *
 from pyquil.api import QVMConnection
 import numpy
 from scipy.stats import chisquare 
+from random import shuffle
 
 def genQUIL(layers): #takes a list of gates in format [[H(1),I(2)],[H(2),I(1)],[CNOT(1,2),Y(3)]] converts to QUIL
 
@@ -20,7 +21,7 @@ def Compare_stats(dist_user,dist_ref): #function takes user and reference distri
     
     return result
 
-def Victory(result,tolerance):  #takes the results of stats test and the required tolerance of success 
+def Test_for_victory(result,tolerance):  #takes the results of stats test and the required tolerance of success 
     success = False
 
     if (result[1] > tolerance):
@@ -39,5 +40,33 @@ def jake_test():  #probably bad practise to put my test here but oh well.
 
     prog = genQUIL(test_circuit)
     res = Compare_stats(test_user,test_ref)
-    suc = Victory(res,0.95)
+    suc = Test_for_victory(res,0.95)
     print (prog,res[1],suc)
+
+def User_circuit(gates,depth,program):#function presents user with available gates and makes them create their circuit layer by layer.
+
+    available_gates=[]
+    available_qubits= range(0,depth)
+    for gate in program.instructions:
+        available_gates.append(gate.name)
+    
+
+    shuffle(available_gates)
+
+    for layer in range(depth):
+        print ('layer',layer)
+        print ('Available gates:')
+        print(available_gates)
+        print ('Available quits:')
+        print(available_qubits)
+
+
+
+test_circuit = [[H(1),I(2)],[H(2),I(1)],[CNOT(1,2),Y(3)]]
+trial_gates = [H(1),CNOT(1,2),Y(3)]
+test_user = [0.8,0.4,0.7]
+test_ref = [0.6,0.3,0.2]
+prog = genQUIL(test_circuit)
+res = Compare_stats(test_user,test_ref)
+suc = Test_for_victory(res,0.95)
+User_circuit(trial_gates,3,prog)
