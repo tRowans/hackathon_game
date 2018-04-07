05d89676245fc118754d052604979890f6b5b80a
Jake <Jake.tommey@gmail.com>
2018-04-07 11:34:38 -0700

    Added a function compare_stats() which takes probability statistics and calculates a chi squared test

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from pyquil.quil import Program
 from pyquil.gates import *
 from pyquil.api import QVMConnection
 import numpy
+from scipy.stats import chisquare 
 
 def genQUIL(layers):
 
@@ -14,8 +15,22 @@ def genQUIL(layers):
     return program
 
 
+def compare_stats(dist_user,dist_ref): #function takes user and reference distributions and compares them.
+
+    result = chisquare(dist_user,dist_ref)
+    
+
+    return result
+
+
+
+
+test_user = [0.8,0.4,0.7]
+test_ref = [0.78,0.39,0.71]
+
 test_circuit = [[H(1),I(2)],[H(2),I(1)],[CNOT(1,2),Y(3)]]
 
 prog = genQUIL(test_circuit)
-
+res = compare_stats(test_user,test_ref)
 print (prog)
+print(res)
